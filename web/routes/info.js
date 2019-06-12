5e1ed7d8aa824043d0529ffa6b3d32ed985b8af2
Paulo Lima <pflima92@gmail.com>
2019-06-12 17:22:00 -0300

    Enrich info endpoint.

--- web/routes/info.js
+++ web/routes/info.js
@@ -3,6 +3,8 @@ const p = require('../../package.json');
 // Retrieves API information
 module.exports = function *() {
   this.body = {
-    version: p.version
+    version: p.version,
+    info: `${p.host}/v1/info`,
+    workspaceSyncHook: `${p.host}/v1/workspaceSyncHook`
   }
 }
